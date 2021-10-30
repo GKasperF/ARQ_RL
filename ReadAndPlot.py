9c0c9e8dd712b5a4960c5d359309663f96812180
GKasperF <gustavo.k.facenda@gmail.com>
2021-10-30 16:25:31 -0400

    Plots lower convex hull
    Takes the lower convex hull of the results and then excludes any non-decreasing point (as it is certainly worse than the previous point).

--- ReadAndPlot.py
+++ ReadAndPlot.py
@@ -1,15 +1,30 @@
 import pickle
-
+import numpy as np
+from LowerBound.BruteForceUtilityFunctions import lower_convex_hull
 with open('Data/AgentRLresults.pickle', 'rb') as f:
     store_results = pickle.load(f)
 
-
-alpha_range = np.arange(0.5, 1.6, 0.1)
 average_transmissions = [store_results[t][1] for t in range(len(store_results))]
 average_recovery = [store_results[t][2] for t in range(len(store_results))]
 
+test = zip(average_transmissions, average_recovery)
+test = list(test)
+test = lower_convex_hull(test)
+i = 1
+while 1:
+	if i == len(test):
+		break
+	point = test[i]
+	previous_point = test[i-1]
+	if point[0] > previous_point[0] and point[1] > previous_point[1]:
+		test.pop(i)
+	else:
+		i+=1
+
+average_transmissions = [test[t][0] for t in range(len(test))]
+average_recovery = [test[t][1] for t in range(len(test))]
+
 average_recovery = [x for _, x in sorted(zip(average_transmissions, average_recovery))]
-alpha_range = [x for _, x in sorted(zip(average_transmissions, alpha_range))]
 average_transmissions.sort()
 
 with open('Data/HeuristicsResults.pickle', 'rb') as f:
@@ -18,14 +33,35 @@ with open('Data/HeuristicsResults.pickle', 'rb') as f:
 average_transmissions_heur = [store_results_heur[t][1] for t in range(len(store_results_heur))]
 average_recovery_heur = [store_results_heur[t][2] for t in range(len(store_results_heur))]
 
+test = zip(average_transmissions_heur, average_recovery_heur)
+test = list(test)
+test = lower_convex_hull(test)
+
+average_transmissions_heur = [test[t][0] for t in range(len(test))]
+average_recovery_heur = [test[t][1] for t in range(len(test))]
+
 average_recovery_heur = [x for _, x in sorted(zip(average_transmissions_heur, average_recovery_heur))]
 average_transmissions_heur.sort()
 
-from BruteForceUtilityFunctions import lower_convex_hull
-
-with open('Data/BruteForceIid.pickle', 'wb') as f:
+with open('Data/BruteForceGESimple.pickle', 'rb') as f:
     store_results_brute_force = pickle.load(f)
 
-convex_hull_results = lower_convex_hull(store_results)
+convex_hull_results = lower_convex_hull(store_results_brute_force)
+i = 0
+while 1:
+    point = convex_hull_results[i]
+    if point[0] < (1/(1 - 0.5) - 0.05):
+        convex_hull_results.pop(i)
+    else:
+        i+=1
+    
+    if i == len(convex_hull_results):
+        break
+
+
 average_transmissions_lb = [convex_hull_results[t][0] for t in range(len(convex_hull_results))]
 average_recovery_lb = [convex_hull_results[t][1] for t in range(len(convex_hull_results))]
+
+import matplotlib.pyplot as plt
+plt.plot(average_transmissions, average_recovery, 'x', average_transmissions_heur, average_recovery_heur, '-sg', average_transmissions_lb, average_recovery_lb, '-or')
+plt.show()
