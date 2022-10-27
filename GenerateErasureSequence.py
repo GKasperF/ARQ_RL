debda4f221a0f0ba0980249d997716349b62f583
GKasperF <gustavo.kasperfacenda@mail.utoronto.ca>
2022-10-26 21:12:48 -0400

    Update GenerateErasureSequence.py

--- GenerateErasureSequence.py
+++ GenerateErasureSequence.py
@@ -3,6 +3,7 @@ import Envs.PytorchEnvironments as Envs
 import copy
 import pickle 
 import numpy as np
+from tqdm import tqdm
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 # class Fritchman():
@@ -18,21 +19,21 @@ device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 # for i in range(Num_Samples):
 #   Channel_Sequence[i] = Channel.step()
 
-Num_Samples = 1000
-batch_size = 1000
+Num_Samples = 1000000
+batch_size = 1
 
 Channel_Sequence = torch.zeros(batch_size, Num_Samples).to(device)
 Current_State = torch.zeros(batch_size, Num_Samples).to(device)
-Channel = Envs.GilbertElliott(0.1, 0.25, 0.05, 1, batch_size).to(device)
+#Channel = Envs.GilbertElliott(0.1, 0.25, 0.05, 1, batch_size).to(device)
 #Channel = Envs.GilbertElliott(0.25, 0.25, 0.0, 1, batch_size).to(device)
 #Channel = Envs.Fritchman(0.1, 0.5, 0.05, 4, batch_size).to(device)
-#Channel = Envs.iidchannel(0.1, batch_size).to(device)
+Channel = Envs.iidchannel(0.1, batch_size).to(device)
 
-for i in range(Num_Samples):
+for i in tqdm(range(Num_Samples)):
   #Current_State[:, i] = Channel.state
   Channel_Sequence[:, i] = Channel.step()
 
-with open('Data/GE_Isolated_Sequence_Example.pickle', 'wb') as f:
+with open('Data/Iid_Sequence_Example_Tests.pickle', 'wb') as f:
     torch.save(Channel_Sequence, f)
 
 # with open('Data/GE_Isolated_Sequence_Example.pickle', 'wb') as f:
